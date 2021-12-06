645c0b919e24140ad77459c0f2ffc46402efc85c
Lysimachos Giatrakis <sgiatrakis@gmail.com>
2021-12-06 16:38:52 +0200

    first commit

---
+++ practicalSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PracticalSwiftUI
+//
+//  Created by Lysimachos Giatrakis on 6/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ practicalSwiftUI/PracticalSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticalSwiftUIApp.swift
+//  PracticalSwiftUI
+//
+//  Created by Lysimachos Giatrakis on 6/12/21.
+//
+
+import SwiftUI
+
+@main
+struct PracticalSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
